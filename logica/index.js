d438abe9ff8fc9d61519fbef6961732bab3b0faf
André Bon Balsemão <andrebalsemao@gmail.com>
2022-05-10 15:39:58 -0300

    Criação do exercício de operadores lógicos.

---
+++ logica/index.js
@@ -0,0 +1,18 @@
+console.log (" EXERCÍCIOS")
+
+const primeiroValor = 3
+const segundoValor = 4
+
+const Soma = primeiroValor + segundoValor
+console.log (Soma)
+
+const primeiroValorM = 3
+const segundoValorM = 5
+
+const Multiplicação = primeiroValorM * segundoValorM /2
+console.log (Multiplicação)
+
+const restoDaDivisao = 234 % 5
+console.log ("restoDaDivisao ", restoDaDivisao)
+
+console.log ("casa"> "oi")
