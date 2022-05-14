29977cd876f7a14189e428f3f6001d2d15fc99cf
André Bon Balsemão <andrebalsemao@gmail.com>
2022-05-14 20:12:33 -0300

    versão com todas as respostas

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -31,8 +31,8 @@ function calculaAreaRetangulo() {
       // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
-      const anoAtual = Number(prompt('Digite o valor da altura do retângulo:')) 
-      const anoNascimento = Number(prompt('Digite o valor da largura do retângulo:'))   
+      const anoAtual = Number(prompt('Qual o ano atual?')) 
+      const anoNascimento = Number(prompt('Qual o ano de seu nascimento?'))   
       imprimeIdade = anoAtual - anoNascimento
       console.log(imprimeIdade)
 }
@@ -117,7 +117,13 @@ function retornaUltimoElemento(array) {
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
 
-  
+      let ultimaPalavra= array[array.length-1]
+      let primeiraPalavra= array[0]
+
+      array[array.length-1]= primeiraPalavra
+      array[0]= ultimaPalavra
+
+      return array
 
 }
 
@@ -125,22 +131,55 @@ function trocaPrimeiroEUltimo(array) {
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
 
+      const primeiraPalavra = string1.toLowerCase()
+      const segundaPalavra = string2.toLowerCase()
+      const vericacao = primeiraPalavra === segundaPalavra
+      return vericacao
+
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
 
+      let anoAtual2 = prompt('Inform o ano atual')
+      let anoNascismentoUsuario = prompt('Qual o ano de seu nascimento?')
+      let anoEmissaoRG = prompt('Qual o ano de emissão do seu RG?')
+      const idadeUsuario = anoAtual2 - anoNascismentoUsuario
+      const validadeRG = anoAtual2 - anoEmissaoRG
+
+      const condicao1 = validadeRG >= 5 && idadeUsuario <= 20
+      const condicao2 = validadeRG >= 10 && idadeUsuario >20 && idadeUsuario <= 50
+      const condicao3 = validadeRG >= 15 && idadeUsuario > 50
+
+      console.log (condicao1 || condicao2 || condicao3)
+
+
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+      const anoBissexto = (ano%400 == 0)
+      const anoBissexto2 = (ano%4==0 && ano%100!=0)
+      return ((ano%400 == 0) || (ano%4==0 && ano%100!=0))
 
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+
+    let perguntaIdade = prompt('Você tem mais de 18 anos?')
+    let perguntaEscolaridade = prompt('Você possui ensino médio completo?')
+    let perguntaDisponibilidade = prompt('Você possui disponibilidade exclusiva durante os horários do curso?')
+    
+    perguntaIdade == "sim"
+    perguntaEscolaridade == "sim" 
+    perguntaDisponibilidade == "sim" 
+
+    let resposta = perguntaIdade == "sim" && perguntaEscolaridade == "sim" && perguntaDisponibilidade == "sim"
+
+     console.log (resposta) 
 
 }
