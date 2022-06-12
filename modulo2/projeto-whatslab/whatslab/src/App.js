7b79da9a1c916002a15ff951063694de13e6c496
andrebalsemao <andrebalsemao@gmail.com>
2022-06-12 18:10:57 -0300

    Projeto WhatsLab - não funcional, sem formatação

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    usuarios: [],
+    inputNome: "",
+    inputMensagem: ""
+  };
+
+  pegarValorInputNome = (event) =>
+    this.setState({ inputNome: event.target.value });
+  pegarValorinputMensagem = (event) =>
+    this.setState({ inputMensagem: event.target.value });
+
+  addPessoa = () => {
+    const usuario = {
+      nome: this.state.inputNome,
+      Mensagem: this.state.inputMensagem
+    };
+    const novaListaDeUsuarios = [...this.state.usuarios, usuario];
+    this.setState({ usuarios: novaListaDeUsuarios });
+    console.log(this.state.usuarios);
+  };
+
+
+  render() {
+    const mapearLista = this.state.usuarios.map((usuario) => {
+      return (
+        <div className="container">
+          <p>{usuario.nome}</p>
+          <p>{usuario.Mensagem}</p>
+          
+        </div>
+      );
+    });
+    return (
+      <div className="App">
+        <label htmlFor="nome">Nome: </label>
+        <input
+          type="text"
+          id="nome"
+          value={this.state.inputNome}
+          onChange={this.pegarValorInputNome}
+        />
+
+        <br />
+
+        <label htmlFor="Mensagem">Mensagem: </label>
+        <input
+          type="text"
+          id="Mensagem"
+          value={this.state.inputMensagem}
+          onChange={this.pegarValorinputMensagem}
+        />
+
+        <button onClick={this.addPessoa}>Adicionar</button>
+        {mapearLista}
+      </div>
+    );
+  }
 }
-
-export default App;
