fab25eadbd0a7b41e7bfa797234c71ccaf9aacfa
Gabriel Mina da Silva <gabrielminadasilva@MacBook-Air-de-Gabriel.local>
2022-06-15 10:51:16 -0300

    deploy do heroku

---
+++ src/services/getFullAdress.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { Address } from "../types/typeAddress"
+
+export const getFullAdress = async (cep:string):Promise<Address | undefined> => {
+    try {
+       
+        const result = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+
+        if(result){
+            const address: Address = {
+                logradouro: result.data.logradouro,
+                bairro: result.data.bairro,
+                cidade: result.data.localidade,
+                estado: result.data.uf
+            }
+            
+            return address
+        }else{
+            return undefined
+        }
+       
+    } catch (error) {
+       return undefined
+    }
+}
