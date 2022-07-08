28e7e75f7f52a6b7596c477ed2e5816de6d47d7f
andrebalsemao <andrebalsemao@gmail.com>
2022-07-08 17:40:27 -0300

    Astrodev sem estilização

--- modulo3/astromatch/src/App.js
+++ modulo3/astromatch/src/App.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import InitialCard from "./components/InitialCard";
+import { MatchList } from "./components/MatchList";
 
 function App() {
+  const [tela, setTela] = useState("Inicio");
+
+  const TrocaTela = () => {
+    switch (tela) {
+      case "Inicio":
+        return <InitialCard switchTela={switchTela} />;
+      default:
+        return <MatchList switchTela={switchTela} />;
+    }
+  };
+  const switchTela = (arg) =>{
+    setTela(arg)
+
+  }
   return (
     <div className="App">
-      <InitialCard />
+      <TrocaTela />
     </div>
   );
 }
--- modulo3/astromatch/src/components/InitialCard.js
+++ modulo3/astromatch/src/components/InitialCard.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function InitialCard() {
+function InitialCard(props) {
   const [profile, setProfile] = useState([]);
-  const [click, setClick] = useState(undefined);
 
   const getProfile = () => {
     axios
       .get(
-        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Andre/person"
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/AndreB/person"
       )
       .then((response) => {
         console.log(response.data.profile);
@@ -18,18 +17,57 @@ function InitialCard() {
         console.log(error);
       });
   };
+
+  const givMatch = (choice) => {
+    const body = {
+      id: profile.id,
+      choice: true,
+    };
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/AndreB/choose-person",
+        body
+      )
+      .then((response) => {
+        getProfile();
+        if (response.data.isMatch === true) {
+          
+        }
+        console.log(response.data.isMatch);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     getProfile();
   }, []);
 
   return (
     <div className="App">
-      {profile.name}
-      <img src={profile.photo} alt={profile.name} />
-      <button>OK</button>
-      <button>NÃO</button>
-      <button>Lista match's</button>
-      <button>Limpar</button>
+      <p>
+        {" "}
+        {profile.name} {profile.age}
+      </p>
+      <img src={profile.photo} alt={profile.name} width="350px" />
+      <p>{}</p>
+      <p>{profile.bio}</p>
+      <button
+        onClick={() => {
+          givMatch();
+        }}
+      >
+        OK
+      </button>
+      <button
+        onClick={() => {
+          getProfile();
+        }}
+      >
+        NÃO
+      </button>
+      <button onClick={()=>{props.switchTela("Tela")}}>Lista match's</button>
     </div>
   );
 }
