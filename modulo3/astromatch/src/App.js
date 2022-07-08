76ad861300e3748429e632c3130a95f2f826fac3
andrebalsemao <andrebalsemao@gmail.com>
2022-07-07 23:41:23 -0300

    Astromatch 50%

--- modulo3/astromatch/src/App.js
+++ modulo3/astromatch/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState , useEfect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import InitialCard from "./components/InitialCard";
 
 function App() {
   return (
     <div className="App">
-      Hello World!!
+      <InitialCard />
     </div>
   );
 }
--- modulo3/astromatch/src/components/InitialCard.js
+++ modulo3/astromatch/src/components/InitialCard.js
@@ -1,9 +1,37 @@
-import React, { useState , useEfect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function InitialCard () {
-    const [profile, setProfile] = useState ([])
-    
+function InitialCard() {
+  const [profile, setProfile] = useState([]);
+  const [click, setClick] = useState(undefined);
+
+  const getProfile = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Andre/person"
+      )
+      .then((response) => {
+        console.log(response.data.profile);
+        setProfile(response.data.profile);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  return (
+    <div className="App">
+      {profile.name}
+      <img src={profile.photo} alt={profile.name} />
+      <button>OK</button>
+      <button>NÃO</button>
+      <button>Lista match's</button>
+      <button>Limpar</button>
+    </div>
+  );
 }
 
-
+export default InitialCard;
